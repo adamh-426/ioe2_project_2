611d753dd6d45945dab0d4352aba69485a4fc7da
adamh-426 <78606083+adamh-426@users.noreply.github.com>
2021-03-29 23:36:48 -0400

    Update ioe2_p2_arduino.ino

--- ioe2_p2_arduino/ioe2_p2_arduino.ino
+++ ioe2_p2_arduino/ioe2_p2_arduino.ino
@@ -8,8 +8,8 @@
 #define DEVICE_ID "esp8266"
 #define DEVICE_CREDENTIAL "Z$IKjLz72mk9oZzz"
 
-#define SSID "YOURWIFINAME"
-#define SSID_PASSWORD "WIFIPASS"
+#define SSID "COGECO-2C940"
+#define SSID_PASSWORD "0988DCOGECO"
 
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
@@ -17,32 +17,32 @@ ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 #define LED1 13
 #define LED2 12
 #define BUTTON1 14
-#define BUTTON2 15
+//#define BUTTON2 15
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(BUTTON1, INPUT);
-  pinMode(BUTTON2, INPUT);
+  //pinMode(BUTTON2, INPUT);
 
   thing.add_wifi(SSID, SSID_PASSWORD);
 
   // pin control example (i.e. turning on/off a light, a relay, etc)
   thing["led1"]<< digitalPin(LED1);
   thing["led2"]<< digitalPin(LED2);
-  thing["button1"] >> [](pson& out){
-      int button1 = 0;
-      button1 = digitalRead(BUTTON1);
-      Serial.println(button1);
-      out = button1;
+  thing["input1"] >> [](pson& out){
+      int input1 = 0;
+      input1 = digitalRead(BUTTON1);
+      Serial.println(input1);
+      out = input1;
       };
-  thing["button2"] >> [](pson& out){
-      int button2 = 0;
-      button2 = digitalRead(BUTTON2);
-      Serial.println(button2);
-      out = button2;
-      };
+//  thing["input2"] >> [](pson& out){
+//      int input2 = 0;
+//      input2 = digitalRead(BUTTON2);
+//      Serial.println(input2);
+//      out = input2;
+//      };
 }
 
 void loop() {
